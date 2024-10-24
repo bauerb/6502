026b249ec1b37b23fc9348430265e2980e139282
Bernd Bauer <bernd.bauer@vusion.com>
2024-10-24 14:51:33 +0200

    Added test for stopping emulator on jumping to same address

--- asm/quit_test/quit_test.asm
+++ asm/quit_test/quit_test.asm
@@ -3,17 +3,8 @@
 
 .segment "DATA"
 
-sum1:         .res 1, $4
-sum2:         .res 1, $5
-erg:          .res 1, $0
-
 .segment "CODE"
 .proc main
-
-  lda sum1
-  clc
-  adc sum2
-  sta erg
 
   jmp *
 
